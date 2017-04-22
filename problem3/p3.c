df8f1b1b577100d1631fc06a3e15a5a20795681a
Ezekiel Reyna <eer2138@columbia.edu>
2017-04-22 02:12:36 -0400

    put in comments for what needs to be done for program to work

--- problem3/p3.c
+++ problem3/p3.c
@@ -11,12 +11,16 @@
 #include <string.h>
 #include <math.h>
 
-#define NUM 600851475143
+#define NUMB 600851475143
 
 int main( int argc, char **argv )
 {
 	int largest_prime;
-	int bound = NUM / (int) log( (int) NUM );
+	unsigned long long num = NUMB;
+	// need to replace instances of NUM with num or bound
+	// also need to create a bound function that'll take the
+	// sqrt of an unsigned long long
+	int bound = (int) sqrt( NUM );
 	int prime_factors[ bound ];
 	int i, j;
 
@@ -24,7 +28,7 @@ int main( int argc, char **argv )
 	prime_factors[0] = 1;
 	prime_factors[1] = -1; //-1 used as null terminator
 
-	for( i = 2; i < NUM /2; i++ ) {
+	for( i = 2; i < bound; i++ ) {
 		if( NUM % i == 0 ) {
 			for( j = 1; i % prime_factors[j] != 0; j++ )
 				;
