1e71426bd6e7811792f15beda4287d8090eed95f
Ezekiel Reyna <eer2138@columbia.edu>
2017-04-22 22:07:34 -0400

    put in other lang implementations with scripts and Makefile

---
+++ problem1/vers_java/P1.java
@@ -0,0 +1,20 @@
+public class P1
+{
+        public static void main( String[] args )
+        {
+                int sum;
+                int i;
+                int bound;
+
+                bound = 1000;
+
+                sum = 0;
+                for( i = 0; i < bound; i++ ) {
+                        if( i % 3 == 0 || i % 5 == 0 )
+                                sum += i;
+                }
+
+                System.out.print("\nThe sum of all multiples of 3 and 5 below");
+                System.out.print(" "+bound+" is "+sum+"\n\n");
+        }
+}
