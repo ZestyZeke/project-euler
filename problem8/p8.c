d37f9146e1fb168a09c7ce4d1b840ff0cc9f1034
Ezekiel Reyna <eer2138@columbia.edu>
2017-04-27 17:33:11 -0400

    solved problem 8

--- problem8/p8.c
+++ problem8/p8.c
@@ -12,8 +12,48 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define BUF_SIZE 4096
+#define ADJACENT 4
+
+long long find_product( char *buf_pos, int adj )
+{
+	int i;
+	long long product;
+	product = 1;
+
+	for( i = 0; i < adj; i++ ) {
+		product *= buf_pos[ i ] - '0'; //convert char to int
+	}
+
+	return product;
+}
 
 int main( int argc, char **argv )
 {
+	char buffer[ BUF_SIZE ];
+	FILE *fp;
+	int i;
+	int adj;
+	long long largest_product;
+	long long curr_product;
+
+	(argc == 2) ? (adj = atoi(argv[1])) : (adj = ADJACENT);
+
+	fp = fopen( "num.txt", "r" );
+	fgets( buffer, BUF_SIZE, fp );
+
+	largest_product = -1;
+	for( i = 0; buffer[ i + adj ] != '\0'; i++ ) {
+
+		curr_product = find_product( &buffer[i], adj );
+
+		if( curr_product > largest_product )
+			largest_product = curr_product;
+	}
+
+	printf( "\nThe largest product of the %d adjacent digits are", adj );
+	printf( " %lld\n\n", largest_product );
 	return 0;
 }
