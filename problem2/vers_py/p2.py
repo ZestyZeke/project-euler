6af07deb68e4f74f06be02646e914635fe130849
Ezekiel Reyna <eer2138@columbia.edu>
2017-04-29 16:14:38 -0400

    solved p2 in python

---
+++ problem2/vers_py/p2.py
@@ -0,0 +1,32 @@
+### p2.py
+#
+# Zeke Reyna
+#
+# Sums fibonacci numbers under a certain bound
+import sys
+
+def main( argv ):
+    
+    if len(argv) == 2:
+        bound = int( argv[1] )
+    else:
+        bound = 10
+
+    first = 1
+    second = 1
+    third = 0
+    sum = 0
+
+    while ( third < bound ):
+        if( second % 2 == 0 ):
+            sum += second
+
+        third = first + second
+        first = second
+        second = third
+
+    print( "\nThe sum of even valued fibonacci",
+            " numbers under ", bound, " is ",
+            sum, "\n" )
+
+main( sys.argv )
