85cce1202aca7bc6dc3364eeed6caa0aaab57655
Ezekiel Reyna <eer2138@columbia.edu>
2017-04-29 15:52:47 -0400

    finally got python working

---
+++ problem1/vers_py/p1.py
@@ -0,0 +1,26 @@
+### p1.py
+#
+# Zeke Reyna
+# 
+# This function sums all the multiples of 3 and 5
+# below BOUND
+# hopefully still works
+import sys
+
+def main( argv ):
+    
+    if len(argv) == 2:
+        bound = int( argv[1] )
+    else:
+        bound = 10
+
+    sum = 0
+    for i in range( 1, bound ):
+        if i % 3 == 0 or i % 5 == 0:
+            sum += i
+
+    print( "\nThe sum of all multiples of 3 and 5 below ", bound, " is ", sum , "\n" )
+
+
+
+main( sys.argv )
