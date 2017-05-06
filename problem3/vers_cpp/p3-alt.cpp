d00effefba299b4c8b2cbebb2e61f41732a2b4c4
Ezekiel Reyna <eer2138@columbia.edu>
2017-05-06 14:01:18 -0400

    p3 works with cpp, starting alt version that's more robust

--- problem3/vers_cpp/p3-alt.cpp
+++ problem3/vers_cpp/p3-alt.cpp
@@ -1,5 +1,5 @@
 /*
- * p3.cpp
+ * p3-alt.cpp
  *
  * Zeke Reyna
  *
@@ -23,6 +23,39 @@ using namespace std;
  * the vector? so a vector of 2-vectors?
  */
 
+/*
+ * recursive function, given a number find two factors for it,
+ * and return them as a vector<int>. Should be recursively called,
+ * check if either of two factors is n. If one is, then don't
+ * recursively call self because that means factor is prime
+ * and doesn't need to be reduced furhter.
+ */
+// may have problems with values that are larger than int
+vector<int> find_factors( int n )
+{
+        int i;
+        int first_factor, second_factor;
+        vector<int> retlist;
+
+        first_factor = -1;
+        second_factor = -1;
+        for( i = 2; i < n; i++ ) {
+                if( n % i == 0 ) {
+                        first_factor = i;
+                        second_factor = n / i;
+                        break;
+                }
+        }
+
+        retlist.push_back( first_factor );
+        retlist.push_back( second_factor );
+
+        if( first_factor < 0 )
+                return retlist;
+        else {
+        }
+}
+
 int main( int argc, char **argv )
 {
         unsigned long long i;
@@ -35,8 +68,7 @@ int main( int argc, char **argv )
 
         (argc == 2) ? (num = stoull( argv[1] )) : (num = NUM);
 
-        //bound = num / 2;
-        bound = (int) sqrt( num );
+        bound = num / 2;
 
         for( i = 2; i < bound; i++ ) {
 
