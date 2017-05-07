86563f5606558ecd0a04102c28ed6198c4d4c1eb
Ezekiel Reyna <eer2138@columbia.edu>
2017-05-07 16:58:31 -0400

    solved p8 in c++

--- problem8/vers_cpp/p8.cpp
+++ problem8/vers_cpp/p8.cpp
@@ -12,7 +12,48 @@
 
 #include <iostream>
 
+#define BUF_SIZE 2048
+#define ADJACENT 4
+
+using namespace std;
+
+long long find_product( char *buf_pos, int adj )
+{
+        int i;
+        long long product;
+        product = 1;
+
+        for( i = 0; i < adj; i++ ) {
+                product *= buf_pos[ i ] - '0'; //convert char to int
+        }
+
+        return product;
+}
 int main( int argc, char **argv )
 {
-	return 0;
+        char buffer[ BUF_SIZE ];
+        FILE *fp;
+        int i;
+        int adj;
+        long long largest_product, curr_product;
+
+        (argc == 2) ? (adj = stoi(argv[1])) : (adj = ADJACENT);
+
+        fp = fopen( "../num.txt", "r" );
+        fgets( buffer, BUF_SIZE, fp );
+        fclose( fp );
+
+        largest_product = -1;
+        for( i = 0; buffer[ i + adj ] != '\0'; i++ ) {
+
+                curr_product = find_product( &buffer[i], adj );
+
+                if( curr_product > largest_product )
+                        largest_product = curr_product;
+        }
+
+        cout<<"\nThe largest product of the "<<adj<<" digits in num.txt are";
+        cout<<" "<<largest_product<<endl<<endl;
+
+        return 0;
 }
