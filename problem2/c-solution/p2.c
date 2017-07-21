9b4fad816fe9989d6d110cad4657688dca5b8b15
zeke <eer2138@columbia.edu>
2017-07-20 22:59:15 -0500

    solved p2 in C

--- problem2/c-solution/p2.c
+++ problem2/c-solution/p2.c
@@ -12,13 +12,33 @@
 #include <stdlib.h>
 #define		DEFAULT		4000000
 
+void iterate(int *first, int *second)
+{
+	int third = *first + *second;
+	*first = *second;
+	*second = third;
+}
+
 int main(int argc, char **argv)
 {
 	int bound;
+	int first, second;
+	unsigned long long sum;
 
 	(argc == 2) ? (bound = atoi(argv[1])) : (bound = DEFAULT);
 
-	int first, second, third;
+	// first two fib nums
+	first  = 1;
+	second = 1;
+	sum    = 0;
+
+	for (; second < bound; iterate(&first, &second)) {
+		if (second % 2 == 0)
+			sum += (unsigned long long) second;
+	}
+
+	printf("\nThe sum of the even-valued Fibonacci numbers under ");
+	printf("the bound %d is: %llu\n\n", bound, sum);
 
 	return 0;
 }
